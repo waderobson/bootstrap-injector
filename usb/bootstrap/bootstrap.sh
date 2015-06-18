882d82782707d0efdb2893d67d65fb5f5d4e6b1a
Wade Robson <wrobson@urthecast.com>
2015-06-17 21:24:50 -0700

    Updated with hostname script

--- usb/bootstrap/bootstrap.sh
+++ usb/bootstrap/bootstrap.sh
@@ -9,18 +9,21 @@ HOST='google.ca'
 # ################ MUNKI STUFF #######################
 # URL that points to munkitools pkg
 MUNKI_TOOLS_URL='https://munkibuilds.org/munkitools2-latest.pkg'
-MUNKI_REPO='http://10.0.10.192/munki'
+MUNKI_REPO='http://your.munkiserver.com/munki_repo'
 MUNKI_MANIFEST='general'
-MUNKI_REPORT_SERVER='http://10.0.10.192/munki/report'
+MUNKI_REPORT_SERVER='http://your.munkiserver.com/munki_repo/report'
 
 # ############# CASPER STUFF ######################
 # the URL of your JSS, if you are behind a load balancer you can use the URL and do not put a slash on the end
 # example = https://myjss.company.com:8443
-jssURL=''
+JSS_URL=''
 
 # an inviation code from a quickadd package generated from Recon.app
-invitationCode=''
+INVITATION_CODE=''
 # ##################################################
+
+## Need to wait before installing the Wifi.profile
+sleep 30
 
 # Uncomment if you want to use wifi
 # . tools/wifi/install_wifi.sh
@@ -29,9 +32,10 @@ invitationCode=''
 . tools/clear_reg.sh
 . tools/enable_external_network_adapter.sh
 . tools/disable_apple_icloud_and_diagnostic_pop_ups.sh
+. tools/use_serial_for_hostname.sh
 . tools/wait_for_network.sh
 
-#####  
+################# CHOSE YOUR MGMT ##################  
 # This is where you chose 
 # The management you want to use
 
@@ -40,6 +44,8 @@ invitationCode=''
 
 # Include Casper
 # . casper/jamfBootstrap.sh
+
+##################################################### 
 
 # Clean up
 rm /Library/LaunchDaemons/bsi.launchd.plist
