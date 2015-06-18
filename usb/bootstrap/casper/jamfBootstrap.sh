08194a0eed934a8c3df0fd85322bef0bd2ccc028
Wade Robson <wrobson@urthecast.com>
2015-06-17 21:29:44 -0700

    Cleaning and changed variables to match the rest of the project

--- usb/bootstrap/casper/jamfBootstrap.sh
+++ usb/bootstrap/casper/jamfBootstrap.sh
@@ -9,10 +9,10 @@
 #### start variables defined by the code and user variables
 
 # location of the JAMF binary on any web app
-jamfBinary="${jssURL}/bin/jamf.gz"
+jamfBinary="${JSS_URL}/bin/jamf.gz"
 
 jssConnectionTest() {
-jssTest=$(curl -k -I ${jssURL}/JSSResource/computers/id/1 | awk '/HTTP/ { print $2}')
+jssTest=$(curl -k -I ${JSS_URL}/JSSResource/computers/id/1 | awk '/HTTP/ { print $2}')
 if [[ ${jssTest} == '401' ]]
     then echo "we can connect to the JSS" 
 else echo "JSS is not reachable...exiting"
@@ -37,10 +37,9 @@ gzip -d /tmp/jamf.gz
 mv /tmp/jamf /usr/sbin/jamf
 chown root:wheel /usr/sbin/jamf
 chmod +rx /usr/sbin/jamf
-jamf createConf -k -url ${jssURL}
-jamf enroll -invitation ${invitationCode} -noRecon
+jamf createConf -k -url ${JSS_URL}
+jamf enroll -invitation ${INVITATION_CODE} -noRecon
 }
-
 
 jssConnectionTest
 downloadBinary
@@ -48,31 +47,24 @@ jamfEnroll
 # enable ssh
 systemsetup -setremotelogin on
 
-# Name the computer
-# todo. come up with a universal namer for casper and munki 
-jamf setComputerName -useSerialNumber
-
-# This should probably be done by a package
-jamf createAccount -username admin -realname administrator -password password -admin
-
-
 # resets the all the policies for this machine on the server
 jamf flushPolicyHistory
 
 # Run the inventory
 jamf recon
 
-# enable jamfhelper for initial config.
-# It will let you know is working. At the end of your 
-# configuration policy make sure you kill it and delete it as mentioned 
-# here -> https://jamfnation.jamfsoftware.com/discussion.html?id=24#responseChild151
-# these three lines are needed to stop jamfhelper from loading. Put them at the end of your
-# configuration policy
-# ---------------
-# killall -m jamfHelper                                          
-# rm /Library/LaunchAgents/bsi.jamfhelper.plist             
-# killall loginwindow
-# ----------------
+# # enable jamfhelper for initial config.
+# # It will let you know is working. At the end of your 
+# # configuration policy make sure you kill it and delete it as mentioned 
+# # here -> https://jamfnation.jamfsoftware.com/discussion.html?id=24#responseChild151
+# # these three lines are needed to stop jamfhelper from loading. Put them at the end of your
+# # configuration policy
+# # ---------------
+# # killall -m jamfHelper                                          
+# # rm /Library/LaunchAgents/bsi.jamfhelper.plist             
+# # killall loginwindow
+# # ----------------
+#
 # cp casper/bsi.jamfhelper.plist /Library/LaunchAgents/
 
 
